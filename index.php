4b7980ea79406819681e632a137b6fb6e611c963
Ralph009 <ankyiee@yahoo.com>
2018-07-12 18:23:30 +0000

    Initial 2nd Commit

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <title>
+          PHP SCRIPT
+      </title>  
+    </head>
+</html>
+<body>
+  <h1> 
+
 <?php
 
 echo "Hi";
 
-?>
+$variableOne = "This is a variable";
+
+echo $variableOne;
+
+?>  
+</h1>
+</body>
